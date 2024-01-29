32cdf69d0687c3680c3e26b5efb8fed2242e7c34
Icaro Asas Virtuais <icaro.capobianco@gmail.com>
2024-01-29 13:47:17 -0300

    + pwa

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,15 @@
 function App() {
 
   return (
-    <h1 style={{textAlign: 'center'}}>Vite + React</h1>
+    <div style={{textAlign: 'center'}}>
+      <h1>Asas Virtuais PWA</h1>
+      <h2>Vite + React built as a PWA you can install!</h2>
+      <h3>
+        repository
+        <br/>
+        <a href="https://github.com/icarocapobianco/pwa.asasvirtuais.com">github.com/icarocapobianco/pwa.asasvirtuais.com</a>
+      </h3>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Asas Virtuais PWA',
+        short_name: 'Asas Virtuais PWA',
+        description: 'Asas Virtuais PWA project example',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+  })],
 })
